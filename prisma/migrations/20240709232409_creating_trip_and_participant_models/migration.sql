d5f3d1a6720f0ae6784ff865e1ad101ca1b0fb26
Jorge Lucas <jorgelucastcoronado@gmail.com>
2024-07-09 20:39:10 -0300

    feat: participants in prisma

---
+++ prisma/migrations/20240709232409_creating_trip_and_participant_models/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "trips" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "destination" TEXT NOT NULL,
+    "starts_at" DATETIME NOT NULL,
+    "ends_at" DATETIME NOT NULL,
+    "is_confirmed" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "participants" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "is_confirmed" BOOLEAN NOT NULL DEFAULT false,
+    "is_owner" BOOLEAN NOT NULL DEFAULT false,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "participants_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "trips" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
